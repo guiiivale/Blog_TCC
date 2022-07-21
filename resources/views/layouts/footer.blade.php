90f94ad3fae9f894a1ea8482ac824a4496806df2
Guilherme Vale <83623617+guiiivale@users.noreply.github.com>
2022-07-21 15:36:22 -0700

    Changed footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,7 @@
 <footer class="footer">
     <div class="container-fluid">
         <div class="copyright">
-            &copy; {{ now()->year }} {{ __('made with') }} <i class="fa fa-heart"></i> {{ __('by') }}
-            <a href="https://github.com/guiiivale" target="_blank">{{ __('Guilherme Vale') }}</a>{{','}} 
-            <a href="https://github.com/JonathanMeira" target="_blank">{{ __('Jonathan Meira') }}</a> {{','}} 
-            <a href="https://github.com/pauloportugal1" target="_blank">{{ __('Paulo Portugal') }}</a> {{__('and ')}} 
-            <a href="https://github.com/gulama14" target="_blank">{{ __('Gustavo Benites') }}</a> 
+            &copy; {{ now()->year }} {{ __('made with') }} <i class="fa fa-heart"></i>  
         </div>
     </div>
 </footer>
